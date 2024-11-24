6dbd2afea8c7adad164be48c7c16a1f3129991a9
Wassim <91387114+wassim01110111@users.noreply.github.com>
2024-11-24 01:49:56 +0100

    Update netatmo-api.js

--- netatmo-api.js
+++ netatmo-api.js
@@ -1,133 +1,156 @@
-'use strict';
-var request = require('request');
-var simpleoauth2 = require('simple-oauth2');
+"use strict";
+var request = require("request");
 
 class NetatmoAPI {
-    constructor(clientid, clientsecret) {
-        const credentials = {
-            client: {
-                id: clientid,
-                secret: clientsecret,
-            },
-            auth: {
-                tokenHost: 'https://api.netatmo.com/',
-                tokenPath: '/oauth2/token'
-            },
-            http: { headers: { 'User-Agent': 'homebridge-thermostat-netatmo' } }
-        };
+  constructor(accessToken) {
+    this.token = accessToken;
+  }
 
-        this._oauth2 = simpleoauth2.create(credentials);
-    }
+  getThermostatState() {
+    var deferred = new Promise((resolve, reject) => {
+      request(
+        {
+          url: "https://api.netatmo.com/api/homestatus",
+          qs: { access_token: this.token },
+        },
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var processedData = NetatmoAPI.processData(body);
+            if (!processedData) {
+              reject("No data found.");
+              return;
+            }
 
-    authenticate(username, password) {
-        var tokenConfig = {
-            username: username,
-            password: password,
-            scope: 'read_thermostat write_thermostat'
-        };
-
-        var promise = this._oauth2.ownerPassword
-            .getToken(tokenConfig)
-            .then((result) => {
-                const token = this._oauth2.accessToken.create(result);
-                return token;
-            });
-
-        return promise;
-    }
-
-    getThermostatState(token) {
-        var deffered = new Promise((resolve, reject) => {
-            request({
-                url: 'https://api.netatmo.com/api/homestatus',
-                qs: {'access_token': token.token.access_token}
-            }, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var processedData = NetatmoAPI.procesData(body);
-                    if (!processedData) {
-                        reject('No data found.');
-                        return;
-                    }
-
-                    resolve(processedData);
-                    return;
-                } else {
-                    if (!error && body) {
-                        error = body;
-                    }
-                    var errorstr = 'Error getting thermostat state (status code '+response.statusCode+'): ' + error;
-                    reject(errorstr);
-                    return;
-                }
-            });
-        });
-
-        return deffered;
-    }
-
-    setThermostat(data) {
-        var deffered = new Promise((resolve, reject) => {
-
-            request({
-                url: 'https://api.netatmo.com/api/setroomthermpoint',
-                qs: {
-                    'access_token': data['token'].token.access_token,
-                    'device_id': data['device_id'],
-                    'module_id': data['module_id'],
-                    'setpoint_temp': data['temperature'],
-                    'setpoint_mode': data['mode'],
-                }
-            }, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var processedData = NetatmoAPI.processAPIResponse(body);
-                    if (!processedData) {
-                        reject('Setting thermostate state failed (invalid response)');
-                        return;
-                    }
-
-                    resolve(processedData);
-                    return;
-                } else {
-                    if (!error && body) {
-                        error = body;
-                    }
-                    var errorstr = 'Setting thermostate state failed (status code '+response.statusCode+'): ' + error;
-                    reject(errorstr);
-                    return;
-                }
-
-            });
-        });
-
-        return deffered;
-    }
-
-    static processAPIResponse(text) {
-        var data = JSON.parse(text);
-        if (data.status !== 'ok') {
-            return null;
+            resolve(processedData);
+            return;
+          } else {
+            if (!error && body) {
+              error = body;
+            }
+            var errorstr =
+              "Error getting thermostat state (status code " +
+              response.statusCode +
+              "): " +
+              error;
+            reject(errorstr);
+            return;
+          }
         }
-        return data;
-    }
+      );
+    });
 
-    static procesData(text) {
-        var data = this.processAPIResponse(text);
-        if (!data) {
-            return null;
+    return deferred;
+  }
+
+  setThermostat(data) {
+    var deferred = new Promise((resolve, reject) => {
+      request(
+        {
+          url: "https://api.netatmo.com/api/setroomthermpoint",
+          qs: {
+            access_token: this.token,
+            home_id: data["home_id"],
+            room_id: data["room_id"],
+            temp: data["temperature"],
+            mode: data["mode"],
+          },
+        },
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var processedData = NetatmoAPI.processAPIResponse(body);
+            if (!processedData) {
+              reject("Setting thermostate state failed (invalid response)");
+              return;
+            }
+
+            resolve(processedData);
+            return;
+          } else {
+            if (!error && body) {
+              error = body;
+            }
+            var errorstr =
+              "Setting thermostate state failed (status code " +
+              response.statusCode +
+              "): " +
+              error;
+            reject(errorstr);
+            return;
+          }
         }
-        var device = data.body.devices[0];
-        var thermostat = device.modules[0];
-        var result = {
-            device_id: device._id,
-            module_id: thermostat._id,
-            temperature: thermostat.measured.temperature,
-            heatingOn: (thermostat.therm_relay_cmd !== 0),
-            setPoint: thermostat.measured.setpoint_temp,
-            mode: thermostat.setpoint.setpoint_mode,
-        };
+      );
+    });
 
-        return result;
+    return deferred;
+  }
+
+  setThermoMode(data) {
+    var deferred = new Promise((resolve, reject) => {
+      request(
+        {
+          url: "https://api.netatmo.com/api/setroomthermpoint",
+          qs: {
+            access_token: this.token,
+            home_id: data["home_id"],
+            mode: data["mode"],
+          },
+        },
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var processedData = NetatmoAPI.processAPIResponse(body);
+            if (!processedData) {
+              reject("Setting thermostate state failed (invalid response)");
+              return;
+            }
+
+            resolve(processedData);
+            return;
+          } else {
+            if (!error && body) {
+              error = body;
+            }
+            var errorstr =
+              "Setting thermostate state failed (status code " +
+              response.statusCode +
+              "): " +
+              error;
+            reject(errorstr);
+            return;
+          }
+        }
+      );
+    });
+
+    return deferred;
+  }
+
+  static processAPIResponse(text) {
+    var data = JSON.parse(text);
+    if (data.status !== "ok") {
+      return null;
     }
+    return data;
+  }
+
+  static processData(text) {
+    var data = this.processAPIResponse(text);
+    if (!data) {
+      return null;
+    }
+    var home = data.body.home;
+    var thermostat = home.modules[0];
+    var thermostat2 = home.modules[1];
+    var room = home.rooms[0];
+    var result = {
+      room_id: thermostat.id,
+      temperature: room.therm_measured_temperature,
+      heatingOn: thermostat2.boiler_status,
+      setPoint: room.therm_setpoint_temperature,
+      mode: room.therm_setpoint_mode,
+    };
+
+    return result;
+  }
 }
 
 module.exports = NetatmoAPI;
